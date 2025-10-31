a6f8d9918acefca0ce0ea7ef0ac98dcc43889b50
gone888 <bryanboyle.mail@gmail.com>
2025-10-31 08:35:11 -0400

    Exercise-2.4

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -8,17 +8,23 @@ class Recipe(models.Model):
   cooking_time = models.IntegerField()
   difficulty = models.CharField(max_length=20, blank=True, null=True)
 
+  def return_ingredients_as_list(self):
+    return self.ingredients.split(",")
+
   def calculate_difficulty(self):
     num_ingredients = len(self.return_ingredients_as_list())
     if self.cooking_time < 10 and num_ingredients < 4:
-      self.difficulty = "Easy"
+      return "Easy"
     elif self.cooking_time < 10 and num_ingredients >= 4:
-      self.difficulty = "Medium"
+      return "Medium"
     elif self.cooking_time >= 10 and num_ingredients < 4:
-      self.difficulty = "Intermediate"
-    else:
-      self.difficulty = "Hard"
-    self.save()
+      return "Intermediate"
+    elif self.cooking_time >= 10 and num_ingredients >= 4:
+      return "Hard"
+
+  def save(self, *args, **kwargs):
+    self.difficulty = self.calculate_difficulty()
+    super().save(*args, **kwargs)
 
   def __str__(self):
     return str(self.name)
