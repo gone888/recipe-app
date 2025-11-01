67be5583259dae6c83996adb7c6e3eed5d7320ea
gone888 <bryanboyle.mail@gmail.com>
2025-10-31 20:15:59 -0400

    Configure Postgres database for Heroku

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -84,10 +84,7 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',  # fallback to SQLite
-        conn_max_age=600
-    )
+'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
